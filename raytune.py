52b09a1c6b3d5cc5c546efc923871cf8b9360940
Cong <cong.bai@outlook.com>
2022-11-06 05:07:24 +0900

    small bug fix

--- raytune.py
+++ raytune.py
@@ -14,7 +14,7 @@ class HiddenPrints:
 
     def __enter__(self):
         self._original_stdout = sys.stdout
-        sys.stdout = open(os.devnull, 'w')
+        sys.stdout = open(os.devnull, 'w', encoding="utf8")
 
     def __exit__(self, exc_type, exc_val, exc_tb):
         sys.stdout.close()
